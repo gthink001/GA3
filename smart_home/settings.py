c071516ce10fc3574e3a6e748ac5af89f9d736f0
g-think <lalith.yes@gmail.com>
2023-01-07 22:48:53 +0530

    first

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=5r^yn5(!g-=qhy3%32vdq(f=azvw6))06ww@qr3om8u@qd0-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['*', 'Ga-env.eba-p8ebfmrm.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*', 'Ga-env.eba-p8ebfmrm.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -139,9 +139,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
