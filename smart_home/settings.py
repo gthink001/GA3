e318f557b1e9842d43b3650120cf9ae4b407d422
g-think <lalith.yes@gmail.com>
2023-01-07 19:59:21 +0530

    first

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'zpamqlvg65!G',
+        'HOST': 'database-1.cpiiypusrsra.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
